3bbb8bc31bde56b0a6bfafec9225928f49644a42
bstrefry <brian.trefry@yale.edu>
2026-04-20 18:49:25 -0400

    Update 1.1 4/20 @ 6:50pm
    General Update for VMOC and Resource Page and Central VMOC database

---
+++ vmocs.js
@@ -0,0 +1,43 @@
+const vmocs = [
+  {
+    title: "YSPH VMOC Special Report — Measles, The Americas",
+    date: "2026-04-12",
+    link: "https://www.slideshare.net/slideshow/ysph-vmoc-special-report-measles-the-americas-4-12-2026/286982030"
+  },
+  {
+    title: "YSPH VMOC Special Report — Measles, The Americas",
+    date: "2026-04-05",
+    link: "https://www.slideshare.net/slideshow/ysph-vmoc-special-report-measles-the-americas-4-5-2026/286853741"
+  },
+  {
+    title: "YSPH VMOC Special Report — Measles, The Americas",
+    date: "2026-03-29",
+    link: "https://www.slideshare.net/slideshow/ysph-vmoc-special-report-measles-the-americas-3-29-2026/286740666"
+  },
+  {
+    title: "YSPH VMOC Special Report - Measles - The Americas",
+    date: "2026-03-22",
+    link: "https://www.slideshare.net/slideshow/ysph-vmoc-special-report-measles-the-americas-22-march-2026/286628406"
+  },
+  {
+    title: "YSPH VMOC Special Report — Measles, The Americas",
+    date: "2026-03-15",
+    link: "https://www.slideshare.net/slideshow/ysph-vmoc-special-report-measles-the-americas-3-15-2026/286534348"
+  },
+  {
+    title: "YSPH VMOC Special Report — Measles, The Americas",
+    date: "2026-03-08",
+    link: "https://www.slideshare.net/slideshow/ysph-vmoc-special-report-measles-the-americas-3-8-2026/286412793"
+  },
+  {
+    title: "VMOC Special Report — Measles in America",
+    date: "2026-03-01",
+    link: "https://www.slideshare.net/slideshow/vmoc-special-report-measles-in-america-1-march-2026/286301794"
+  },
+  {
+    title: "Comprehensive Update — Measles Outbreaks and Public Health Response",
+    date: "2026-02-22",
+    link: "https://www.slideshare.net/slideshow/comprehensive-update-on-measles-outbreaks-and-public-health-responses-in-the-americas-2025-2026/286187254"
+  }
+];
+//Add title, date, and link to VMOCs here, will auto update for the most recent verison
